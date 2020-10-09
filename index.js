b1fd30316b17c596d403b3a8bb7bfc936276978a
Elwin Ung <elwin.ung@gmail.com>
2020-10-09 02:02:47 -0400

    Updated building sequence, questions structure, storing arrays and generateMarkdown.js

--- index.js
+++ index.js
@@ -1,145 +1,152 @@
-// import inquirer node_modules
+// import inquirer.js
 var inquirer = require("inquirer");
-// import fs node_modules
+// import fs.js
 var fs = require("fs");
-// import markdown generator
+// import generateMarkdown.js
 var MD = require("./utils/generateMarkdown.js");
 
 // array of questions for user
 const questions = ["What's the title of your project?", 
                    "Please enter the description/abstract for the project",
-                   "Please enter the lists of contents",
                    "Please enter installation instructions",
                    "Please enter usage information",
-                   "Please enter license information",
                    "Please enter contributing guidelines",
                    "Please enter testing guidelines",
-                   "Please enter any links for questions"];
-var build = [];
+                   "Please enter any text intended for the Enquiry section",
+                   "This will be added to the Enquiry sections. \nPlease enter your github username",
+                   "This will be added to the Enquiry sections. \nPlease enter you email address"];
 
-// getting initial infos
+// array of choices for user to select
+const choices = ["Title",
+                 "Project Description",
+                 "Installation_Instruction",
+                 "Usage",
+                 "License",
+                 "Contribution_Guidelines",
+                 "Testing_Guidelines",
+                 "For_Enquiry",
+                 "Add Github Link",
+                 "Add Email Address",
+                 new inquirer.Separator()]
+
+// array of licenses for user to choose from
+const licenseChoices = ["Apache 2.0 License",
+                        "BSD 3-Clause",
+                        "BSD 2-Clause",
+                        "Eclipse Public License 1.0",
+                        "GNU GPL v3",
+                        "GNU GPL v2",
+                        "GNU AGPL v3",
+                        "GNU LGPL v3",
+                        "ISC License",
+                        "The MIT License",
+                        "Mozilla Public License 2.0",
+                        "The Artistic License 2.0",
+                        "The Unlicense",
+                        new inquirer.Separator()];
+
+// for storing all the elements
+// 0 -> Description, 1 -> ToC, 2 -> Installation, 3 -> Usage, 4 -> License, 5 -> Contribution, 6 -> Testing, 7 -> Questions
+var build = [];
+var descriptions = Array(8).fill("");
+var title;
+var badges = [];
+var licenseInfo = [];
+
+
+// asking user which part to be added
 function userSelect(){
-    inquirer.prompt([{
+    inquirer.prompt({
         type: "list",
         message: "Please select the section you want to add",
         name: "userSelect",
-        choices: ["Title",
-                "Project Description",
-                "Table of Contents",
-                "Installation Instruction",
-                "Usage Information",
-                "License",
-                "Contribution Guidelines",
-                "Testing Guidelines",
-                "Links for Questions",
-                new inquirer.Separator()]
-    }]).then(function(response){
+        choices: choices
+    }).then(function(response){
+        build[0] = MD.generateSection("Description");
+        build[2] = MD.generateSection("Installation_Instruction");
+        build[3] = MD.generateSection("Usage");
+        build[5] = MD.generateSection("Contribution_Guidelines");
+        build[6] = MD.generateSection("Testing_Guidelines");
+        build[7] = MD.generateSection("For_Enquiry");
         switch (response.userSelect){
-            case "Title":{
-                userPrompt(0);
-                break;
-            }
-            case "Project Description":{
-                userPrompt(1);
-                build[1] = MD.generateSection("Description");
-                break;
-            }
-            case "Table of Contents":{
-                userPrompt(2);
-                build[3] = MD.generateSection("Table of Contents");
-                break;
-            }
-            case "Installation Instruction":{
-                userPrompt(3);
-                build[5] = MD.generateSection("Installation");
-                break;
-            }
-            case "Usage Information":{
-                userPrompt(4);
-                build[7] = MD.generateSection("Usage");
-                break;
-            }
-            case "License":{
-                userPrompt(5);
-                build[9] = MD.generateSection("License");
-                break;
-            }
-            case "Contribution Guidelines":{
-                userPrompt(6);
-                build[11] = MD.generateSection("Contributing");
-                break;
-            }   
-            case "Testing Guidelines":{
-                userPrompt(7);
-                build[13] = MD.generateSection("Tests");
-                break;
-            }
-            case "Links for Questions":{
-                userPrompt(8);
-                build[15] = MD.generateSection("Questions");
-                break;
-            }
-            default:{
-                break;
-            }
+            case "Title": userPrompt(0); break;
+            case "Project Description": userPrompt(1); break;
+            case "Installation_Instruction": userPrompt(2); break;
+            case "Usage": userPrompt(3); break;
+            case "License": licensePrompt(); break;
+            case "Contribution_Guidelines": userPrompt(4); break;
+            case "Testing_Guidelines": userPrompt(5); break;
+            case "For_Enquiry": userPrompt(6); break;
+            case "Add Github Link": userPrompt(7); break;
+            case "Add Email Address": userPrompt(8); break;
+            default: break;
         }
     });
 }
 
-async function userPrompt(index){
+// asking user for details based on choice in userSelect()
+function userPrompt(index){
     inquirer.prompt({
         type: "input",
         message: questions[index],
-        name: "userInput"
+        name: "userInput",
+        default: "Default PlaceHolder"
     }).then(function(response){
+        // checking if input is empty
         if(response.userInput == "") {
             console.log("Input cannot be empty");
-            morePrompt();
+            morePrompt(); // sending user back to the menu by asking if there is more to add
             return;
         }
         switch (index){
-            case 0:{
-                build[0] = MD.generateTitle(response.userInput);
-                break;
-            }
-            case 1:{
-                build[2] = MD.generateBody(response.userInput);
-                break;
-            }
-            case 2:{
-                build[4] = MD.generateTitle(response.userInput);
-                break;
-            }
-            case 3:{
-                build[6] = MD.generateTitle(response.userInput);
-                break;
-            }
-            case 4:{
-                build[8] = MD.generateTitle(response.userInput);
-                break;
-            }
-            case 5:{
-                build[10] = MD.generateTitle(response.userInput);
-                break;
-            }
-            case 6:{
-                build[12] = MD.generateTitle(response.userInput);
-                break;
-            }
-            case 7:{
-                build[14] = MD.generateTitle(response.userInput);
-                break;
-            }
-            case 8:{
-                build[16] = MD.generateTitle(response.userInput);
-                break;
-            }
+            case 0: title = MD.generateTitle(response.userInput); break;
+            case 1: descriptions[0] = MD.generateBody(response.userInput); break; // Project Description
+            case 2: descriptions[2] = MD.generateBody(response.userInput); break; // Installation
+            case 3: descriptions[3] = MD.generateBody(response.userInput); break; // Usage
+            case 4: descriptions[5] = MD.generateBody(response.userInput); break; // Contribution
+            case 5: descriptions[6] = MD.generateBody(response.userInput); break; // Testing
+            case 6: descriptions[7] = MD.generateBody(response.userInput); break; // Questions
+            case 7: descriptions[7] += MD.generateGitLink(response.userInput); break;
+            case 8: descriptions[7] += MD.generateBody(response.userInput); break;
+            default: break;
         }
-        morePrompt();
+        morePrompt(); // asking user if there is more to add
     })
 }
 
+// asking user to choose the licenses they wanted to add
+function licensePrompt(){
+    var index;
+    inquirer.prompt({
+        type:"rawlist",
+        message: "Please select the license you would like to add",
+        name: "licenseChoice",
+        choices: licenseChoices
+    }).then(function(response){
+        switch (response.licenseChoice){
+            case "Apache 2.0 License": index = 0; break;
+            case "BSD 3-Clause": index = 1; break;
+            case "BSD 2-Clause": index = 2; break;
+            case "Eclipse Public License 1.0": index = 3; break;
+            case "GNU GPL v3": index = 4; break;
+            case "GNU GPL v2": index = 5; break;
+            case "GNU AGPL v3": index = 6; break;
+            case "GNU LGPL v3": index = 7; break;
+            case "ISC License": index = 8; break;
+            case "The MIT License": index = 9; break;
+            case "Mozilla Public License 2.0": index = 10; break;
+            case "The Artistic License 2.0": index = 11; break;
+            case "The Unlicense": index = 12; break;
+        }
+        badges.push(MD.generateBadge(index)); // pushing the selected badges onto the stack
+        licenseInfo.push(MD.generateLicenseInfo(index)); // pushing the selected license info onto the stack
+        morePrompt(); // asking user if there is more to add
+    });
+}
+
+// asking user if there is more to add
 function morePrompt(){
+    console.log("Please note that by saying No, the program will end and a file named temp.md will be created or overwritten.");
     inquirer.prompt({
         type: "confirm",
         message: "Do you want to add more sections?",
@@ -147,28 +154,72 @@ function morePrompt(){
     }).then(function(response){
         if(response.confirm) userSelect();
         else {
-            writeToFile("temp.md", build);
+            createFile("temp.md");// creating file to write or overwritting
+            writeToFile("temp.md"); // building the README
             console.log("Thank you for using this README generator! \nYour README is in temp.md.");
         }
     })
 }
 
-// function to write README file
-function writeToFile(fileName, data) {
-    console.log(build);
-    for(let i=0; i < data.length; i++){
-        if(data[i] != null) fs.appendFileSync(fileName, data[i], "utf8");
+// creating a file to put the generated README
+function createFile(fileName){
+    console.log("creating files...");
+    fs.writeFile(fileName, "", (err)=>{
+        if(err) console.log(`Error creating file: ${err}`);
+    });
+}
+
+// building README.md by iterating throught the "build" array
+function writeToFile(fileName) {
+    fs.accessSync(fileName, (err) => {
+        if(err){
+            console.log("creating files...");
+            fs.writeFile(fileName, "", (err)=>{
+                if(err) console.log(`Error creating file: ${err}`);
+            });
+        }
+    });
+    // writing title
+    console.log("writing title...")
+    if(title != undefined) fs.appendFileSync(fileName, title);
+    else console.log("[title]: Title is undefined");
+    // adding badges
+    console.log("adding badges...")
+    for(let i=0; i < badges.length; i++){
+        if(badges[i] != null) fs.appendFileSync(fileName, badges);
     }
+    // writing sections sequentially
+    for(let i=0; i < build.length; i++){
+        // build[1] = ToC, build[4] = License; They are left empty to trigger inserting ToC and License 
+        // writing ToC
+        if(i == 1){
+            console.log("writing ToC...");
+            let temp = MD.generateSection("Table of Contents");
+            fs.appendFileSync(fileName, temp);
+            temp = MD.generateList(choices.slice(2, 8));
+            fs.appendFileSync(fileName, temp);
+        // writing license
+        } else if (i == 4){
+            console.log("writing License...");
+            let temp = MD.generateSection("License");
+            fs.appendFileSync(fileName, temp);
+            licenseInfo.forEach(item => fs.appendFileSync(fileName, item));
+        // writing sections sequentially
+        } else {
+            console.log("writing other sections...");
+            fs.appendFileSync(fileName, build[i]);
+            fs.appendFileSync(fileName, descriptions[i]);
+        }
+
+    }
+    
 }
 
 // function to initialize program
-async function init() {
-    console.log("")
-    // creating a file to put the generated README
-    fs.writeFileSync("temp.md", "", (err)=>{
-        console.log(`Error creating file: ${err}`);
-    });
-    userSelect();
+function init() {
+
+    console.log("This is not an editor and will overwrite any previous outputs/files named temp.md. \nPlease remove/save any files named temp.md before proceeding.");
+    userSelect(); // starting sequence
 }
 
 // function call to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,16 +3,107 @@ function generateTitle(title) {
   return `# ${title}`;
 }
 
-function generateBody(description){
-  return `\n${description} <br>`;
-}
-
 function generateSection(sectionName){
   return `\n## ${sectionName}`;
 }
 
+function generateBody(description){
+  return `\n${description} <br>`;
+}
+
+function generateGitLink(username){
+  return `\nhttps://github.com/${username}`;
+}
+
+function generateList(items){
+  var tempString = "";
+  items.forEach(element => {
+    let temp = element.charAt(0).toUpperCase() + element.slice(1);
+    tempString += `\n[${temp}](#${element})`
+  });
+  return tempString;
+}
+
+function generateBadge(index){
+  var badges = ["\n[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://www.apache.org/licenses/LICENSE-2.0)",
+                "\n[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)",
+                "\n[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)",
+                "\n[![License](https://img.shields.io/badge/License-EPL%202.0-red.svg)](https://www.eclipse.org/legal/epl-2.0/)",
+                "\n[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+                "\n[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)",
+                "\n[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)",
+                "\n[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)",
+                "\n[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)",
+                "\n[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
+                "\n[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)",
+                "\n[![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)](https://opensource.org/licenses/Artistic-2.0)",
+                "\n[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)"];
+  return badges[index];
+}
+
+function generateLicenseInfo(index){
+  var year = new Date().getFullYear();
+  var info = [`\n**Apache License 2.0**<br>
+               \nLicensed under the Apache License, Version 2.0 (the "License") you may not use this file except in compliance with the License. You may obtain a copy of the License at: http://www.apache.org/licenses/LICENSE-2.0 <br>
+               \nUnless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License. <br>`,
+              `\n**BSD 3-Clause**<br>
+               \nRedistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: <br>
+               \n1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. <br>
+               \n2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. <br>
+               \n3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission. <br>
+               \nTHIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. <br>`,
+              `\n**BSD 2-Clause**<br>
+               \nRedistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met: <br>
+               \n1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer. <br>
+               \n2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution. <br>
+               \nTHIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. <br>`,
+              `\n**Eclipse Public License 1.0**<br>
+               \nPlease refer to this link: https://www.eclipse.org/legal/epl-2.0/ <br>`,
+              `\n**GNU GPL v3**<br>
+               \nCopyright (C) <${year}> <br>
+               \nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. <br>
+               \nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. <br>
+               \nYou should have received a copy of the GNU General Public License along with this program.  If not, see <https://www.gnu.org/licenses/gpl-3.0.html>. <br>`,
+              `\n**GNU GPL v2**<br>
+               \nCopyright (C) ${year} <br>
+               \nThis program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 2 of the License, or (at your option) any later version. <br>
+               \nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. <br>
+               \nYou should have received a copy of the GNU General Public License along with this program; If not, see https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html. <br>`,
+              `\n**GNU AGPL v3**<br>
+               \nCopyright (C) <${year}> <br>
+               \nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. <br>
+               \nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details. <br>
+               \nYou should have received a copy of the GNU Affero General Public License along with this program.  If not, see <https://www.gnu.org/licenses/>. <br>`,
+              `\n**GNU LGPL v3**<br>
+               \nPlease refer to this link: https://www.gnu.org/licenses/lgpl-3.0 <br>`,
+              `\n**ISC License**<br>
+               \nCopyright <${year}> <br>
+               \nPermission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies. <br>
+               \nTHE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. <br>`,
+              `\n**MIT License**<br>
+               \nCopyright <${year}> <br>
+               \nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: <br>
+               \nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. <br>
+               \nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. <br>`,
+              `\n**Mozilla Public License 2.0**<br>
+               \nPlease refer to this link: https://www.mozilla.org/en-US/MPL/2.0/ <br>`,
+              `\n**Artistic License 2.0**<br>
+               \nPlease refer to this link: https://opensource.org/licenses/Artistic-2.0 <br>`,
+              `\n**Unlicense**<br>
+               \nThis is free and unencumbered software released into the public domain. <br>
+               \nAnyone is free to copy, modify, publish, use, compile, sell, or distribute this software, either in source code form or as a compiled binary, for any purpose, commercial or non-commercial, and by any means. <br>
+               \nIn jurisdictions that recognize copyright laws, the author or authors of this software dedicate any and all copyright interest in the software to the public domain. We make this dedication for the benefit of the public at large and to the detriment of our heirs and successors. We intend this dedication to be an overt act of relinquishment in perpetuity of all present and future rights to this software under copyright law. <br>
+               \nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. <br>
+               \nFor more information, please refer to http://unlicense.org/ <br>`];
+  return info[index];
+}
+
 module.exports = {
   generateTitle: generateTitle,
+  generateSection: generateSection,
   generateBody: generateBody,
-  generateSection: generateSection
+  generateList: generateList,
+  generateGitLink: generateGitLink,
+  generateBadge: generateBadge,
+  generateLicenseInfo: generateLicenseInfo
 }
